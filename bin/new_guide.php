372dd4540d6075b7c309c301d6d3e67e3fae66ef
Jeremy Pry <jeremy.pry@gmail.com>
2015-08-14 17:52:37 -0400

    Update template creation script

--- bin/new_guide.php
+++ bin/new_guide.php
@@ -1,6 +1,9 @@
 #!/usr/bin/env php
 <?php
 
+use Symfony\Component\Console\Application;
+use JPry\Choir_Guide\Commands\Template_Create;
+
 define('ROOT_DIR', dirname(__DIR__));
 date_default_timezone_set('America/New_York');
 
@@ -9,7 +12,7 @@ require_once(ROOT_DIR .'/vendor/autoload.php');
 $data = json_decode(ROOT_DIR . '/composer.json');
 $version = isset($data->version) ? $data->version : 'dev';
 
-$app = new \Symfony\Component\Console\Application('choir_guide', $version);
-$app->add(new \JPry\Choir_Guide\Commands\Template_Create());
-
+// Do the Thing!
+$app = new Application('choir_guide', $version);
+$app->add(new Template_Create());
 $app->run();
